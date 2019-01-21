184dc9f10d10b2cc5122922ba038acbb4f6dc7a8
varshaneya <varshaneya@users.noreply.github.com>
2019-01-21 15:39:35 +0530

    for cifar folder

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .resnet import *
+from .resenet import *
+from .seresnet import *
---
+++ seresnet.py
@@ -0,0 +1,95 @@
+'''
+Ported from https://github.com/moskomule/senet.pytorch
+'''
+
+import torch.nn as nn
+from torchvision.models import ResNet
+from models.cifar.se_module import SELayer
+
+__all__ = ['seresnet']
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+class CifarSEBasicBlock(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, reduction=16):
+        super(CifarSEBasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.se = SELayer(planes, reduction)
+
+        if inplanes != planes:
+            self.downsample = nn.Sequential(nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False),
+                                            nn.BatchNorm2d(planes))
+        else:
+            self.downsample = lambda x: x
+        self.stride = stride
+
+    def forward(self, x):
+        residual = self.downsample(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.se(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class CifarSEResNet(nn.Module):
+    def __init__(self, depth, num_classes=10, reduction=16):
+        super(CifarSEResNet, self).__init__()
+        assert (depth - 2) % 6 == 0, 'depth should be 6n+2'
+        n_size = (depth - 2) //6
+        self.inplane = 16
+        self.conv1 = nn.Conv2d(3, self.inplane, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplane)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(CifarSEBasicBlock, 16, blocks=n_size, stride=1, reduction=reduction)
+        self.layer2 = self._make_layer(CifarSEBasicBlock, 32, blocks=n_size, stride=2, reduction=reduction)
+        self.layer3 = self._make_layer(CifarSEBasicBlock, 64, blocks=n_size, stride=2, reduction=reduction)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(64, num_classes)
+        self.initialize()
+
+    def initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride, reduction):
+        strides = [stride] + [1] * (blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inplane, planes, stride, reduction))
+            self.inplane = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def seresnet(**kwargs):
+    return CifarSEResNet(**kwargs)
